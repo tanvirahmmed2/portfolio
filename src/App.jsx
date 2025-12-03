6d2b0844dc540c66ac1d04ad7340d5d21d47f8d1
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-04 01:17:01 +0600

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import About from './component/About'
+import Home from './component/Home'
+import Skill from './component/Skill'
+import Project from './component/Project'
+import Projects from './component/Projects'
 
 function App() {
   return (
     <div className='w-full relative overflow-x-hidden bg-gray-200'>
-      <Navbar/>
+      <Navbar />
+      <div className='w-full mt-20'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/skill' element={<Skill />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/project/:[name]' element={<Project />} />
+        </Routes>
+      </div>
+
     </div>
   )
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <section className='w-full fixed top-0 bg-gradient-to-br from-blue-500 to-purple-400 text-white'>
-        <nav className='w-full flex flex-col items-center justify-around p-4'>
+        <nav className='w-full flex flex-row items-center justify-around px-4 h-20'>
             <a href='/'>
                 <h1 className='text-3xl font-semibold'>Tanvir Ahmmed</h1>
                 <p className='text-sm'>Full Stack Web Developer</p>

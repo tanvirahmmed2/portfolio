83423a221ab753bb27775232ee526ffe15c983fa
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-09 11:49:20 +0600

    api

--- src/app/api/project/route.js
+++ src/app/api/project/route.js
@@ -1,6 +1,7 @@
 import { database } from "@/lib/mongoose";
 import Project from "@/models/project";
 import { NextResponse } from "next/server";
+import slugify from "slugify";
 
 
 export async function POST(req) {
@@ -10,7 +11,52 @@ export async function POST(req) {
         const formData = await req.formData();
 
         const title = formData.get("title");
-        
+        const description = formData.get('description')
+        const tags = formData.get('tags')
+        const category = formData.get('category')
+        const siteLink = formData.get('siteLink')
+        const repository = formData.get('repository')
+        const skills = formData.get('skills')
+        const imageFile = formData.get('image')
+
+
+        if (!title || !description || !tags || !category || !siteLink || !repository || !skills) {
+            return NextResponse.json({
+                success: false,
+                message: "Please fill all inputs"
+            }, { status: 400 })
+        }
+
+        if (!imageFile) {
+            return NextResponse.json({
+                success: false,
+                message: "Please upload image"
+            }, { status: 400 })
+        }
+
+        const imageBuffer = Buffer.from(await imageFile.arrayBuffer());
+
+        const cloudImage = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream(
+                { folder: "portfolio" },
+                (err, result) => {
+                    if (err) reject(err);
+                    else resolve(result);
+                }
+            );
+            stream.end(imageBuffer);
+        });
+
+        const slug = slugify(title)
+
+        const existProject = await Project.findOne({ slug })
+        if (existProject) {
+            return NextResponse.json({
+                success: false,
+                message: "Please use another title"
+            }, { status: 400 })
+        }
+
 
 
 

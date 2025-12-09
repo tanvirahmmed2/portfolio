3e08937aa05a6f9b5dc3b74bc2d4c9e052f5a5c1
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-09 13:05:55 +0600

    project

--- src/app/api/project/[slug]/route.js
+++ src/app/api/project/[slug]/route.js
@@ -1,8 +1,47 @@
+import { database } from "@/lib/mongoose";
+import Project from "@/models/project";
+import { NextResponse } from "next/server";
 
 
 
 
-export async function GET({params}) {
-    con
-    
+export async function GET(req, { params }) {
+    try {
+        await database()
+
+        const tempSlug = await params
+        const slug = tempSlug.slug
+
+        if (!slug) {
+            return NextResponse.json({
+                success: false,
+                message: 'Slug not found'
+            }, { status: 400 })
+        }
+
+        const project = await Project.findOne({ slug })
+
+        if (!project) {
+            return NextResponse.json({
+                success: false,
+                message: "Project data not found"
+            }, { status: 400 })
+        }
+
+        return NextResponse.json({
+            success: true,
+            message: "Successfully fetched project data",
+            payload: project
+        }, { status: 200 })
+
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: "Failed to fecth project data",
+            error: error.message
+        }, { status: 500 })
+
+    }
+
 }
--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -1,9 +1,52 @@
+import { BASE_URL } from '@/lib/secure'
+import Image from 'next/image'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Project = () => {
+const Project = async({params}) => {
+  const tempSlug= await params
+  const slug= tempSlug.slug
+
+  const res= await fetch(`${BASE_URL}/api/project/${slug}`, {
+    method: "GET",
+    cache: 'no-store',
+     credentials: 'include'
+  })
+  const data= await res.json()
+  const project= data.payload
+  if(!data || data.length <1){
+    return redirect('/projects')
+  }
+
+  const {title, description, image, skills, tags, category, repository, siteLink,}= project
+  
   return (
-    <div>
-      
+    <div className='w-full md:w-3/4 lg:w-1/2 mx-auto flex flex-col items-center justify-center gap-8 p-4 bg-white py-6'>
+      <h1 className='text-2xl font-semibold text-center'>{project.title}</h1>
+      <Image src={image} alt={title} width={1000} height={1000} className='w-full rounded-2xl shadow-sm'/>
+      <p>Category: {category}</p>
+      <p className='w-full text-justify'><span className='italic opacity-25'> Description:   </span>{description}</p>
+      <div className='w-full flex flex-wrap gap-2'>
+        {
+          skills.map((skill)=> (
+            <p key={skill} className='px-2 bg-black/20 rounded-2xl text-white '>{skill}</p>
+          ))
+        }
+      </div>
+
+      <div className='w-full flex flex-wrap gap-2'>
+        {
+          tags.map((tag)=> (
+            <p key={tag} className='px-2 bg-black/20 rounded-2xl text-white '>{tag}</p>
+          ))
+        }
+      </div>
+      <div className='w-full flex flex-row items-center justify-between gap-4'>
+        <Link className='w-full bg-sky-500 text-white text-center rounded-2xl' href={repository}>Github</Link>
+        <Link className='w-full bg-sky-500 text-white text-center rounded-2xl' href={siteLink}>Live Preview</Link>
+      </div>
+
     </div>
   )
 }
